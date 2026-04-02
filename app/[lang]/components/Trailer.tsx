af34802a67432830237c46c2428abba8cb62655b
MyNa Do <myna.do@bimobject.com>
2026-04-02 16:13:06 +0200

    fix: console warning

--- app/[lang]/components/Trailer.tsx
+++ app/[lang]/components/Trailer.tsx
@@ -12,7 +12,6 @@ export default function Trailer() {
         <div className="w-full mx-auto relative h-0 pb-[56.25%]">
           <iframe
             src="https://player.vimeo.com/video/1166689842?h=cc03a48e4c&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
-            allowFullScreen
             allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share"
             referrerPolicy="strict-origin-when-cross-origin"
             loading="lazy"
